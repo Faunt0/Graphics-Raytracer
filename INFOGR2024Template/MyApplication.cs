1d25e739ad42134fa5931e2c65ce2784798a084b
Lex Klaassen <kllex028@gmail.com>
2024-05-21 04:40:00 +0200

    working on reflections

--- INFOGR2024Template/MyApplication.cs
+++ INFOGR2024Template/MyApplication.cs
@@ -2,6 +2,8 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using static System.Formats.Asn1.AsnWriter;
+using System.Net;
 
 namespace Template
 {
@@ -73,16 +75,21 @@ namespace Template
     }
     class Primitive
     {
-        public Vector3 color; // alpha? doubles?
-        public Vector3 specular_color;
+        public Vector3 color; // alpha? doubles? Maybe use Color4 object
+        public Vector3 specular_color; // is niet nodig
         public float specularity;
+        public Material material;
 
         public void SetSpecularity(Vector3 specular_color, float specularity)
         {
             this.specular_color = specular_color;
             this.specularity = specularity;
         }
-
+        public class Material
+        {
+            // This needs to have multiple sub-classes which define properties
+            public Tuple<float, float, float> spec_ref_mat;
+        }
         public class Sphere : Primitive
         {
             public Vector3 position;
@@ -122,8 +129,10 @@ namespace Template
     {
         public Primitive[] primitives;
         public Light[] lights;
+        public int max_bounces;
         public Scene()
         {
+            max_bounces = 8;
             primitives = new Primitive[] { 
                 new Primitive.Sphere((2, 0, 4), 0.5f, (1, 0, 0)), 
                 new Primitive.Sphere((-2, 0, 4), 0.4f, (0, 1, 0)), 
@@ -137,7 +146,46 @@ namespace Template
             //primitives[0].SetSpecularity((1, 1, 1), 10);
 
             //lights = new Light[] { new Light(new(4, 1, 1), (1, 1, 1)) };
-            lights = new Light[] { new Light(new(3, 1, 1), (20, 20, 20)) };
+            lights = new Light[] { new Light(new(3, 1, 1), (290, 20, 20)) };
+        }
+        public Vector3 Trace(Ray r, Scene s)
+        {
+            Vector3 color = new Vector3(0, 0, 0);
+            Intersection ins = r.GetIntersection(s);
+            if (ins.nearestP != null)
+            {
+                if (ins.nearestP.specular_color == (1, 1, 1)) // does (1, 1, 1) mean its a pure specular
+                {
+
+                    Vector3 ins_point = r.startPos + ins.distance * r.direction;
+                    Vector3 norm_reflected_dir = r.direction - 2 * Vector3.Dot(r.direction, ins.normal) * ins.normal;
+                    Ray reflected_ray = new Ray(norm_reflected_dir, ins_point);
+                    
+                    if (r.num_of_bounces < s.max_bounces)
+                    {
+                        reflected_ray.num_of_bounces = r.num_of_bounces + 1;
+                        color = ins.nearestP.color * Trace(reflected_ray, s);
+                    }
+                }
+                else
+                {
+                    // calculate the phong diffuse shading etc
+
+                    // calculate the point of the intersection
+                    Vector3 ins_point = r.startPos + ins.distance * r.direction;
+
+                    // illumination ray
+                    Vector3 ill_ray = new Vector3(s.lights[0].position - ins_point);
+
+                    // make the shadow ray
+                    Vector3 shadow_ray = Vector3.Normalize(ill_ray);
+
+                    
+
+                    color = 
+                }
+            }
+            return color;
         }
     }
     class Intersection
@@ -157,10 +205,63 @@ namespace Template
         public Vector3 startPos;
         public Vector3 direction; // is normalised
         public float intersection_dist;
+        public int num_of_bounces;
         public Ray(Vector3 pos, Vector3 dir)
         {
             startPos = pos;
             direction = dir;
+        }
+        public Intersection GetIntersection(Scene s)
+        {
+            Intersection ins = new Intersection();
+
+            foreach (Primitive prim in s.primitives)
+            {
+                // this must be possible in a more efficient and clean way.
+                if (prim is Primitive.Sphere)
+                {
+                    Primitive.Sphere p = (Primitive.Sphere)prim;
+
+                    // Use math
+                    Vector3 C = p.position;
+                    float a = Vector3.Dot(direction, direction); // wanneer is dit niet 1?
+                    float b = -2f * Vector3.Dot(direction, C - startPos);
+                    float c = Vector3.Dot(C - startPos, C - startPos) - p.radius * p.radius;
+
+                    // calculate the discriminant
+                    float D = (float)Math.Pow(b, 2) - 4 * a * c;
+
+                    // only consider the case when there is an intersection
+                    if (D >= 0)
+                    {
+                        // calculate the closest distance to the sphere
+                        float t = Math.Min((-b + (float)Math.Sqrt(D)) / (2 * a), (-b - (float)Math.Sqrt(D)) / (2 * a));
+
+                        // only store the shortest distance and thus the nearest Primitive
+                        if (ins.distance > t || ins.nearestP == null)
+                        {
+                            ins.distance = t;
+                            ins.nearestP = p;
+                        }
+                    }
+                }
+                if (prim is Primitive.Plane)
+                {
+                    Primitive.Plane p = (Primitive.Plane)prim;
+
+                    if (Vector3.Dot(direction, p.normal) != 0)
+                    {
+                        float t = Vector3.Dot((p.p0 - startPos), p.normal) / Vector3.Dot(direction, p.normal);
+                        if (t > 0 && (ins.distance > t || ins.nearestP == null))
+                        {
+                            ins.distance = t;
+                            ins.nearestP = p;
+                        }
+                    }
+                }
+            }
+
+            return ins;
         }
     }
     class Raytracer
@@ -246,7 +347,7 @@ namespace Template
                 // get the color of the pixel
                 if (ins.nearestP != null)
                 {
-                    // start black?
+                    // start black
                     Vector3 col = new Vector3(0, 0, 0);
 
                     // calculate the point of the intersection
@@ -298,7 +399,6 @@ namespace Template
                     // make colours
                     if (!occluded)
                     {
-                        // introduce diffuse materials
                         Vector3 normal = new Vector3();
                         if (ins.nearestP is Primitive.Sphere)
                         {
@@ -307,34 +407,18 @@ namespace Template
                         }
                         else if (ins.nearestP is Primitive.Plane)
                         {
-                            Primitive.Plane p = (Primitive.Plane)ins.nearestP;
+                            Primitive.Plane  p = (Primitive.Plane)ins.nearestP;
                             normal = p.normal;
                         }
                         Vector3 r = Vector3.Normalize(ill_ray - 2 * (Vector3.Dot(ill_ray, normal)) * normal);
 
 
-
-
-                        //Vector3 L_part1 = (1 / (shadow_ray.Length * shadow_ray.Length)) * 
-                        //    Math.Max(0, Vector3.Dot(normal, shadow_ray) / (normal.Length * norm_ray_dir.Length)) *
-                        //    ins.nearestP.color;
-
-                        //Vector3 L_part2 = (1 / (shadow_ray.Length * shadow_ray.Length)) * 
-                        //    (float)Math.Pow(Math.Max(0, Vector3.Dot(norm_ray_dir, r)), ins.nearestP.specularity) *
-                        //    ins.nearestP.specular_color;
-                        //col = EntryWiseMultiply(scene.lights[0].intensity, L_part1);
-
-                        //col += EntryWiseMultiply(scene.lights[0].intensity, L_part2);
-
-
-
-                        // dit werk niet optimaal, zitten wat rare artifacts in. Overflow namelijk
+                        
                         Vector3 sumparts =
                             Math.Max(0, Vector3.Dot(normal, shadow_ray)) * ins.nearestP.color + 
                             (float)Math.Pow(Math.Max(0, Vector3.Dot(norm_ray_dir, r)), ins.nearestP.specularity) * ins.nearestP.specular_color;
                         Vector3 L = EntryWiseMultiply(scene.lights[0].intensity, sumparts * (1 / (ill_ray.Length * ill_ray.Length))) + ins.nearestP.color * (0.2f, 0.2f, 0.2f);
 
-                        //col = (Math.Min(1, L.X), Math.Min(1, L.Y), Math.Min(1, L.Z));
                         col = Vector3.Clamp(L, (0, 0, 0), (1, 1, 1));
 
 
